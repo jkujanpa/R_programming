b43ce6d229a490b88d4fb1a6bf0b7a5497381742
jkujanpa <jyrki.kujanpaa@gmail.com>
2015-07-17 18:01:25 +0300

    Comments added

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,46 +1,46 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  ## 'pollutant' is a character vector of length 1 indicating
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-
     
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; either "sulfate" or "nitrate".
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    # Init values vector
     values <- NULL
   
+    # Go through selected mnitor ids
     for(i in id) {
         
-        print(i)
-        
+        # Generaate filename for file
         monitor <- formatC(i, width = 3, flag = "0")
         filename <- paste(monitor, "csv", sep = ".")
         filename <- paste(directory, filename, sep = "/")
-        
-        print(filename)
-
+        # Read the file 
         data <- read.csv(filename)
-        
+
+        # Store the measurement values of given pollutant
         if (pollutant == "sulfate") {
             data <- data[,2]
         } else { #nitrate
             data <- data[,3]
         }
         
+        # Skip NA values
         data <- data[complete.cases(data)]
+        # Add new values to vector
         values <- c(values, data)
     }
   
-  
     
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
-  ## NOTE: Do not round the result!
-  
-  
+    ## Return the mean of the pollutant across all monitors list
+    ## in the 'id' vector (ignoring NA values)
+    ## NOTE: Do not round the result!
+    
+    # Calculate the mean of all values
     mean(values)  
-  
 }
